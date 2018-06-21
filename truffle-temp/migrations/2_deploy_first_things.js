21cb587601ccae9ebf62029ab55404c7ce3876b6
GauthamN <gauthamnekk@gmail.com>
2018-06-21 17:05:39 +0200

    Changes to truffle migrations file

--- truffle-temp/migrations/2_deploy_first_things.js
+++ truffle-temp/migrations/2_deploy_first_things.js
@@ -1,5 +1,26 @@
-var EthToken = artifacts.require("./PreminedAsset.sol");
+// const ourConfig = require("./ourConfig.js");
+const fs = require('fs');
+import * as masterConfig from "../../utils/config/environment";
 
-module.exports = function(deployer) {
-  deployer.deploy(EthToken);
+const PreminedAsset = artifacts.require("./PreminedAsset.sol");
+const Governance = artifacts.require("./Governance.sol");
+
+const tokenInfoFileName = "./tokenInfo.config";
+
+module.exports = function(deployer, network, accounts) {
+  deployer.then(async () => {
+    const config = masterConfig[network];
+    if (network === "development") {
+      const ethToken = await deployer.new(PreminedAsset);
+      const mlnToken = await deployer.new(PreminedAsset);
+      const eurToken = await deployer.new(PreminedAsset);
+      const tokenAddresses = {
+        "EthToken": ethToken.address,
+        "MlnToken": mlnToken.address,
+        "EurToken": eurToken.address
+      };
+      fs.writeFileSync(tokenInfoFileName, JSON.stringify(tokenAddresses, null, '  '));
+      await deployer.deploy(Governance, [accounts[0]], 1, 100000);
+    }
+  });
 };
